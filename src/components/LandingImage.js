9c87cbfc024ada0918a47dd8fdebcb26c371c581
blauks <brooks.jonathan1@gmail.com>
2019-06-29 16:15:41 +0200

    Startet on the topbar styling

--- src/components/LandingImage.js
+++ src/components/LandingImage.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import "./LandingImageCSS.css"
+import React from "react";
+import "./LandingImageCSS.css";
 
 const LandingImage = () => {
-    return (
-      <div className="LandingImage">
+  return (
+    <div className="LandingImage">
       <h2>Nicolai A. Dalaaker</h2>
-      <img src="https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/58374956_1256833031133034_2416005497267159040_n.jpg?_nc_cat=105&_nc_oc=AQlq1DRz8g2dOg4iS3R33ap878koPpDslXclFYvYtjB2M4uOFc3sZHe-V6dmnOl2MbyDd1UxYzYRgpQWw4xVBNUT&_nc_ht=scontent-vie1-1.xx&oh=eac9431a8016bdd5ca5d756acaaaaafd&oe=5DBF065C">
-      </img>
-
-      </div>
-    );
+      <div class="fadeFromTop" />
+      <img src="https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/58374956_1256833031133034_2416005497267159040_n.jpg?_nc_cat=105&_nc_oc=AQlq1DRz8g2dOg4iS3R33ap878koPpDslXclFYvYtjB2M4uOFc3sZHe-V6dmnOl2MbyDd1UxYzYRgpQWw4xVBNUT&_nc_ht=scontent-vie1-1.xx&oh=eac9431a8016bdd5ca5d756acaaaaafd&oe=5DBF065C" />
+    </div>
+  );
 };
 
 export default LandingImage;
